1fe38aa355cdab4246965289a7d5c88341e380d2
Carlos J Pinheiro <carlos.pnx@gmail.com>
2022-11-17 16:42:31 -0300

    cria metodos para cadastro e visualizacao de seres

--- Main.java
+++ Main.java
@@ -5,7 +5,14 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		SerVivo listaSeres = new SerVivo();
 		
+		listaSeres.cadastraSer(1, "filo", "classe", "ordem", "familia", "genero", "especie");
+		listaSeres.cadastraSer(1, "filo111", "classe1111", "orde111m", "famil111a", "ge111nero", "es111pecie");
+		listaSeres.cadastraSer(1, "fil222o", "cl2222sse", "ord222em", "fami222lia", "gene2222ro", "especi2222e");
+		
+		
+		listaSeres.mostraSeres(1);
 		
 		
 	}
--- SerVivo.java
+++ SerVivo.java
@@ -1,16 +1,143 @@
 package desafioSerVivo.SerVivo;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class SerVivo {
 	
-	AnimaliaEspecie animaliaEspecie;
+	Scanner scan = new Scanner(System.in);
 	
-//	FungiEspecie fungiEspecie;
-//	PlantaeEspecie plantaeEspecie;
-//	MoneraEspecie moneraEspecie;
-//	ProtistaEspecie protistaEspecie;
+	ArrayList<AnimaliaEspecie> listaAnimalia = new ArrayList<>();
+//	ArrayList<FungiEspecie> listaFungi = new ArrayList<>();
+//	ArrayList<PlantaeEspecie> listaPlantae = new ArrayList<>();
+//	ArrayList<MoneraEspecie> listaMonera = new ArrayList<>();
+//	ArrayList<ProtistaEspecie> listaProtista = new ArrayList<>();
 	
-	public void cadastraSer() {
-		
+	
+	public SerVivo() {
+		super();
 	}
+	
+	public void cadastraSer(int reino, String filo, String classe, String ordem, String familia, String genero, String especie) {
+		switch (reino) {
+		case 1:
+			listaAnimalia.add(new AnimaliaEspecie());
+			this.listaAnimalia.get(listaAnimalia.size()-1).setFilo(filo);
+			this.listaAnimalia.get(listaAnimalia.size()-1).setClasse(classe);
+			this.listaAnimalia.get(listaAnimalia.size()-1).setOrdem(ordem);
+			this.listaAnimalia.get(listaAnimalia.size()-1).setFamilia(familia);
+			this.listaAnimalia.get(listaAnimalia.size()-1).setGenero(genero);
+			this.listaAnimalia.get(listaAnimalia.size()-1).setEspecie(especie);
+			break;
+//		case 2:
+//			listaFungi.add(new FungiEspecie());
+//			this.listaFungi.get(listaFungi.size()-1).setFilo(filo);
+//			this.listaFungi.get(listaFungi.size()-1).setClasse(classe);
+//			this.listaFungi.get(listaFungi.size()-1).setOrdem(ordem);
+//			this.listaFungi.get(listaFungi.size()-1).setFamilia(familia);
+//			this.listaFungi.get(listaFungi.size()-1).setGenero(genero);
+//			this.listaFungi.get(listaFungi.size()-1).setEspecie(especie);
+//			break;
+//		case 3:
+//			listaPlantae.add(new PlantaeEspecie());
+//			this.listaPlantae.get(listaPlantae.size()-1).setFilo(filo);
+//			this.listaPlantae.get(listaPlantae.size()-1).setClasse(classe);
+//			this.listaPlantae.get(listaPlantae.size()-1).setOrdem(ordem);
+//			this.listaPlantae.get(listaPlantae.size()-1).setFamilia(familia);
+//			this.listaPlantae.get(listaPlantae.size()-1).setGenero(genero);
+//			this.listaPlantae.get(listaPlantae.size()-1).setEspecie(especie);
+//			break;
+//		case 4:
+//			listaMonera.add(MoneraEspecie());
+//			this.listaMonera.get(listaMonera.size()-1).setFilo(filo);
+//			this.listaMonera.get(listaMonera.size()-1).setClasse(classe);
+//			this.listaMonera.get(listaMonera.size()-1).setOrdem(ordem);
+//			this.listaMonera.get(listaMonera.size()-1).setFamilia(familia);
+//			this.listaMonera.get(listaMonera.size()-1).setGenero(genero);
+//			this.listaMonera.get(listaMonera.size()-1).setEspecie(especie);
+//			break;
+//		case 5:
+//			listaProtista.add(ProtistaEspecie());
+//			this.listaProtista.get(listaProtista.size()-1).setFilo(filo);
+//			this.listaProtista.get(listaProtista.size()-1).setClasse(classe);
+//			this.listaProtista.get(listaProtista.size()-1).setOrdem(ordem);
+//			this.listaProtista.get(listaProtista.size()-1).setFamilia(familia);
+//			this.listaProtista.get(listaProtista.size()-1).setGenero(genero);
+//			this.listaProtista.get(listaProtista.size()-1).setEspecie(especie);
+//			break;
+		default:
+			break;
+		}
+	}
+
+	public void mostraSeres(int reino) {
+		switch (reino) {
+		case 1:
+			for (int i = 0; i < listaAnimalia.size(); i++) {
+				System.out.println("--===--===--===--===--");
+				System.out.println("Reino: "+listaAnimalia.get(i).getReino());
+				System.out.println("Filo: "+listaAnimalia.get(i).getFilo());
+				System.out.println("Classe: "+listaAnimalia.get(i).getClasse());
+				System.out.println("Ordem: "+listaAnimalia.get(i).getOrdem());
+				System.out.println("Família: "+listaAnimalia.get(i).getFamilia());
+				System.out.println("Gênero: "+listaAnimalia.get(i).getGenero());
+				System.out.println("Espécie: "+listaAnimalia.get(i).getEspecie());
+			}
+			break;
+//		case 2:
+//			for (int i = 0; i < listaFungi.size(); i++) {
+//				System.out.println("--===--===--===--===--");
+//				System.out.println("Reino: "+listaFungi.get(i).getReino());
+//				System.out.println("Filo: "+listaFungi.get(i).getFilo());
+//				System.out.println("Classe: "+listaFungi.get(i).getClasse());
+//				System.out.println("Ordem: "+listaFungi.get(i).getOrdem());
+//				System.out.println("Família: "+listaFungi.get(i).getFamilia());
+//				System.out.println("Gênero: "+listaFungi.get(i).getGenero());
+//				System.out.println("Espécie: "+listaFungi.get(i).getEspecie());
+//			}
+//			break;
+//		case 3:
+//			for (int i = 0; i < listaPlantae.size(); i++) {
+//				System.out.println("--===--===--===--===--");
+//				System.out.println("Reino: "+listaPlantae.get(i).getReino());
+//				System.out.println("Filo: "+listaPlantae.get(i).getFilo());
+//				System.out.println("Classe: "+listaPlantae.get(i).getClasse());
+//				System.out.println("Ordem: "+listaPlantae.get(i).getOrdem());
+//				System.out.println("Família: "+listaPlantae.get(i).getFamilia());
+//				System.out.println("Gênero: "+listaPlantae.get(i).getGenero());
+//				System.out.println("Espécie: "+listaPlantae.get(i).getEspecie());
+//			}
+//			break;
+//		case 4:
+//			for (int i = 0; i < listaMonera.size(); i++) {
+//				System.out.println("--===--===--===--===--");
+//				System.out.println("Reino: "+listaMonera.get(i).getReino());
+//				System.out.println("Filo: "+listaMonera.get(i).getFilo());
+//				System.out.println("Classe: "+listaMonera.get(i).getClasse());
+//				System.out.println("Ordem: "+listaMonera.get(i).getOrdem());
+//				System.out.println("Família: "+listaMonera.get(i).getFamilia());
+//				System.out.println("Gênero: "+listaMonera.get(i).getGenero());
+//				System.out.println("Espécie: "+listaMonera.get(i).getEspecie());
+//			}
+//			break;
+//		case 5:
+//			for (int i = 0; i < listaProtista.size(); i++) {
+//				System.out.println("--===--===--===--===--");
+//				System.out.println("Reino: "+listaProtista.get(i).getReino());
+//				System.out.println("Filo: "+listaProtista.get(i).getFilo());
+//				System.out.println("Classe: "+listaProtista.get(i).getClasse());
+//				System.out.println("Ordem: "+listaProtista.get(i).getOrdem());
+//				System.out.println("Família: "+listaProtista.get(i).getFamilia());
+//				System.out.println("Gênero: "+listaProtista.get(i).getGenero());
+//				System.out.println("Espécie: "+listaProtista.get(i).getEspecie());
+//			}
+//			break;
+		default:
+			break;
+		}
+	}
+	
+	
+	
 	
 }
