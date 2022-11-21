30f44528b499ebb316bf603ba5a10b74fe42e034
Laila Gebhard <lailagebhard@gmail.com>
2022-11-21 14:14:07 -0300

    correção família plantae

--- PlantaeFamilia.java
+++ PlantaeFamilia.java
@@ -1,12 +1,9 @@
 package desafioSerVivo.SerVivo;
 
-<<<<<<< HEAD
+
 public class PlantaeFamilia extends PlantaeOrdem {
-=======
-public class PlantaeFamilia {
->>>>>>> 4076e85818ca270fd7faf610e60d6b8670f634dd
-	
-	private String ordem;
+
+	private String familia;
 	
 	public String getFamilia() {
 		return familia;
